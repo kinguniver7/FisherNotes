f86bfc86bbc8f339fa309fd85b459dbc9cbab0d6
Maksym Kravchuk <malpinsoft@gmail.com>
2019-10-05 21:06:17 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { BackpackComponent } from './components/backpack/backpack.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    DashboardComponent,
+    BackpackComponent
   ],
   imports: [
     BrowserModule,
